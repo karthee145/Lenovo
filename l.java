942615d6af10ecd1bb8c646a00676dadfb446d61
tkarthee145 <karthik.nani5@gmail.com>
2019-03-11 22:05:03 +0530

    changes updated

--- l.java
+++ l.java
@@ -1,5 +1,5 @@
 class First {
   public static void main(String[] arguments) {
-    System.out.println("Let's do something using Java technology.");
+    System.out.println("Wellcome to Devops Practice session");
   }
 }
