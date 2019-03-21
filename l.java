45ec5c74abbd681c514bb53d6b079eedd4af8a64
karthee145 <45926701+karthee145@users.noreply.github.com>
2019-03-21 18:18:02 +0530

    l.java
    changes

--- l.java
+++ l.java
@@ -1,5 +1,48 @@
-class First {
-  public static void main(String[] arguments) {
-    System.out.println("Wellcome to Devops Practice session");
-  }
-}
+jab tak tujhe pyaar se
+beinteha main bhar na doon
+jab tak main duaaoon sa
+sau dafa tujhe paDh na loon
+
+until I fill you completely with love
+until I read you a hundred times like prayers,
+
+haan mere paas tum raho
+jaane ki baat na karo
+mere saath tum raho
+jaane ki baat na karo
+
+stay close to me,
+don't talk about leaving.
+stay with me,
+don't talk about leaving.
+
+tum aa gaye baazuon mein mere
+sau savere liye
+sau savere liye
+
+you have come in my arms,
+taking a hundred mornings.
+
+haan baadalon se utaara gaya
+tumko mere liye
+sirf mere liye
+
+you have been brought down from the clouds,
+for me, just for me.
+
+jab tak meri ungliyaan
+tere baalon se kuch keh na lein
+jab tak teri lehar mein
+khwahishein meri beh na lein
+
+until my fingers say something
+to your hair.
+until my wishes flow a bit
+in your waves.
+
+haan mere paas tum raho
+jaane ki baat na karo
+mere saath tum raho
+jaane ki baat na karo
+
+wo...
